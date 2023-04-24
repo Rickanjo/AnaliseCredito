0b4a4014430be398a8b0ef6e8063e53b24ab6fcf
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-24 20:53:15 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Um cliente quer solicitar um empréstimo. Receba o valor solicitado, a quantidade de parcelas desejada e a renda mensal comprovada.
+Console.WriteLine("--- Empréstimo ---");
+
+Console.Write("");
+double valorSolicitado, rendaMensal;
+int qtdeParcelas;
+
+Console.Write("Digite o valor solicitado: R$ ");
+valorSolicitado = double.Parse(Console.ReadLine()!);
+
+Console.Write("Digite a quantidade de parcelas desejada: ");
+qtdeParcelas = int.Parse(Console.ReadLine()!);
+
+Console.Write("Digite a renda mensal comprovada: R$ ");
+rendaMensal = double.Parse(Console.ReadLine()!);
+
+double valorParcela = valorSolicitado / qtdeParcelas;
+double limiteParcela = rendaMensal * 0.3;
+
+if (valorParcela <= limiteParcela) {
+    Console.WriteLine("Empréstimo autorizado!");
+    
+} else {
+    Console.WriteLine("Empréstimo não autorizado.");
+}
